0c898a662def42fb5cf0a9630e55a5cd1e3da3da
arun.prajapati <arunkp1122@gmail.com>
2026-01-06 22:36:42 +0545

    feat: add standalone output configuration to Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   /* config options here */
   allowedDevOrigins: ['http://localhost:3000', 'http://192.168.1.91:3000', '192.168.1.91'],
   // cacheComponents: true, // Disabled to fix dynamic route build issue
